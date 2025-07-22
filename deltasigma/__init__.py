e1c0cfeccc4a049cbb50bfb83c1807617109af82
Ashbir Aviat Fadila <fadila@ashbir.com>
2025-07-22 21:10:39 +0900

    Fix SyntaxWarning by using raw strings for docstrings

--- deltasigma/__init__.py
+++ deltasigma/__init__.py
@@ -12,7 +12,7 @@
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # LICENSE file for the licensing terms.
-"""
+r"""
 *A port of the* **MATLAB Delta Sigma Toolbox** *based on free software and
 very little sleep.*
 
