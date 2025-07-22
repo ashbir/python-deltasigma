f813ac02ed8eeff494fe38c11db3880646b876f9
Ashbir Aviat Fadila <fadila@ashbir.com>
2025-07-22 21:30:09 +0900

    Fix: resolve running about the blas header file issue

--- deltasigma/_config.py
+++ deltasigma/_config.py
@@ -33,15 +33,34 @@ itn_limit = 500
 _debug = False
 
 # get blas information to compile the cython extensions
-# In modern numpy, distutils is removed. We will rely on the build system
-# to find the necessary headers. This section is simplified.
 blas_info = {}
-
+if len(blas_info) == 0 and _debug:
+    warn("Numpy did not detect the BLAS library in the system")
 # Let's make an educated guess
+guessed_include = None
 if 'linux' in sys.platform or 'darwin' in sys.platform:
-    guessed_include = '/usr/include'
-else:
-    guessed_include = None
+    # Common locations for cblas.h on Linux and macOS
+    search_dirs = [
+        '/usr/include',
+        '/usr/include/cblas',
+        '/usr/include/openblas',
+    ]
+    # Add architecture-specific paths for Debian/Ubuntu derivatives
+    if 'linux' in sys.platform:
+        try:
+            import platform
+            arch = platform.machine()
+            arch_dir = '/usr/include/%s-linux-gnu' % arch
+            if os.path.isdir(arch_dir):
+                search_dirs.append(arch_dir)
+        except ImportError:
+            pass  # platform module not available, skip
+
+    for d in search_dirs:
+        if os.path.isfile(os.path.join(d, 'cblas.h')):
+            guessed_include = d
+            break
+
 # wrap it up: numpy or user-set environment var or a lucky guess on our side is
 # needed to get the cblas.h header path. If not found, simulateDSM() will use
 # a CPython implementation (slower).
@@ -52,8 +71,7 @@ setup_args = {"script_args":(["--compiler=mingw32"]
 setup_args = {"script_args":[]}
 lib_include = [np.get_include()]
 if "include_dirs" not in blas_info and "BLAS_H" not in os.environ and \
-   'nt' not in os.name and (not guessed_include or \
-     not os.path.isfile(os.path.join(guessed_include, 'cblas.h'))):
+   'nt' not in os.name and not guessed_include:
     warn("Cannot find the path for 'cblas.h'. You may set it using the environment variable "
          "BLAS_H.\nNOTE: You need to pass the path to the directories were the "
          "header files are, not the path to the files.")
